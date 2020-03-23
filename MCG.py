8b5e3168eaa32d6bd886a3d336cc6c4cc41ad358
sanderorb <sander.orbons@hotmail.com>
2020-03-23 16:56:59 +0100

    Update MCG.py

--- MCG.py
+++ MCG.py
@@ -30,11 +30,11 @@ def mcg(m_f, situation, unit):
 
     # Fuel Moment as from Appendix E
     list_M_f = np.array([298.16, 591.18, 879.08, 1165.42, 1448.40, 1732.53, 2014.80, 2298.84, 2581.92, 2866.30, 3150.18, 3434.52, 3718.52, 4003.23, 4287.76, 4572.24, 4856.56, 5141.16, 5425.64, 5709.90, 5994.04, 6278.47, 6562.82, 6846.96, 7131.00, 7415.33, 7699.60, 7984.34, 8269.06, 8554.05, 8839.04, 9124.80, 9410.62, 9696.97, 9983.40, 10270.08, 10556.84, 10843.87, 11131.00, 11418.20, 11705.50, 11993.31, 12281.18, 12,569.04, 12856.86, 13144.73, 13432.48, 13720.56, 14008.46, 14320.34])
-    M_f = list_M_f[int(m_f/100)] 
+    M_f = list_M_f[int((-m_f+m_bf)/100)] 
     
     # compute Moment and Weight
     W = sum(m_pax)+m_BEM+m_bf-m_f 
-    M = sum(m_pax*x_seat) + m_BEM*x_BEM+M_f
+    M = sum(m_pax*x_seat) + m_BEM*x_BEM+M_f*100
     
     # convert to metric units if needed [N & m]
     if unit != 0:
@@ -43,5 +43,6 @@ def mcg(m_f, situation, unit):
     
     # compute x_c.g.
     X_cg = M/W
-    
+    print(M_f)
+    print(m_f)
     return W, M, X_cg
